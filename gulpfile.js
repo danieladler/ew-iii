24c4b98bc96e049ec910ee8b1a7775fd63783386
danieladler <weston.adler@gmail.com>
2017-02-19 08:29:33 -0800

    install & @import Neat

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var nodemon = require('gulp-nodemon');
 var sass = require('gulp-ruby-sass');
+var neat = require('node-neat').includePaths;
 var autoprefixer = require('gulp-autoprefixer');
 var livereload = require('gulp-livereload');
 
+var paths = {
+  scss: 'public/stylesheets/scss/*.scss'
+}
+
 gulp.task('styles', function() {
-  return sass('public/stylesheets/scss/*.scss', { style: 'expanded' })
+  return sass(paths.scss, {
+    loadPath: [paths.scss].concat(neat),
+    style: 'expanded'
+  })
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('public/stylesheets'))
-    .pipe(livereload());
+    .pipe(livereload())
 });
 
 gulp.task('ejs',function(){
