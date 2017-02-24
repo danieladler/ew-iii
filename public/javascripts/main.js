398399cbac54a0dfde1f03686eadd5ca978a4974
danieladler <weston.adler@gmail.com>
2017-02-24 13:11:41 -0800

    add animation to button

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-  // console.log('yep, JS is working');
-
   // NB: starter code for modal that overlays entire window
   const modal = $('.modal');
 
@@ -12,10 +10,26 @@ $(document).ready(function(){
     $('.modal').removeClass('active');
   });
 
+  // paperfold.js for fold effect
 	var paperfold = $('.content--foldable').paperfold({
 		folds: 3,
 		duration: 500
 	});
 
-	$('.button--fold').click(paperfold.toggle);
+  var button = $('.button--fold');
+
+	button.click(paperfold.toggle);
+
+  // shake button to nudge users into clicking
+  function shakeButton() {
+    button.addClass('shake');
+    setTimeout(function() {
+      button.removeClass('shake');
+    }, 1000);
+  }
+
+  setInterval(function() {
+    shakeButton();
+  }, 2000);
+
 });
