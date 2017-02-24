692fa6ccefb5ca19a281c4c6500775a02c83ee40
danieladler <weston.adler@gmail.com>
2017-02-24 10:21:39 -0800

    add explanatory modal

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
   // console.log('yep, JS is working');
 
   // NB: starter code for modal that overlays entire window
-  // const modal = $('.modal');
-  //
-  //  $('.modal__trigger').click(function(e) {
-  //   modal.addClass('active');
-  // });
-  //
-  // $('.modal__close').click(function() {
-  //   $('.modal').removeClass('active');
-  // });
+  const modal = $('.modal');
+
+   $('.modal__trigger').click(function(e) {
+    modal.addClass('active');
+  });
+
+  $('.modal__close').click(function() {
+    $('.modal').removeClass('active');
+  });
 
 	var paperfold = $('.content--foldable').paperfold(
     {folds: 5}
