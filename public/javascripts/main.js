db2174656a44127b4b9e567d8dcb4c3c283e2e08
danieladler <weston.adler@gmail.com>
2017-02-23 16:03:23 -0800

    add paperfold effect for large-up

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-  console.log('yep, JS is working');
+  // console.log('yep, JS is working');
 
   // NB: starter code for modal that overlays entire window
-  const modal = $('.modal');
+  // const modal = $('.modal');
+  //
+  //  $('.modal__trigger').click(function(e) {
+  //   modal.addClass('active');
+  // });
+  //
+  // $('.modal__close').click(function() {
+  //   $('.modal').removeClass('active');
+  // });
 
-   $('.modal__trigger').click(function(e) {
-    modal.addClass('active');
-  });
+	var paperfold = $('.content--foldable').paperfold(
+    {folds: 5}
+  );
 
-  $('.modal__close').click(function() {
-    $('.modal').removeClass('active');
-  });
+	$('.button--fold').click(paperfold.toggle);
 });
